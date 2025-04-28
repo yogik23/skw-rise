056b8fc54bc2dc8886899fa69a804678ca7942da
yogik23 <61941105+yogik23@users.noreply.github.com>
2025-04-28 15:47:31 +0700

    Update inari.js

--- inari.js
+++ inari.js
@@ -2,6 +2,19 @@ const { ethers } = require('ethers');
 const fs = require("fs");
 const path = require("path");
 const chalk = require('chalk');
+const { displayskw } = require('./skw/displayskw');
+
+const {
+  WETH_ADDRESS,
+  USDT_ADDRESS,
+  inari_ROUTER,
+  erc20_abi,
+  inari_abi,
+  delay,
+  wdETHAmount,
+  borrowUSDT,
+  supplyWETH,
+} = require('./skw/config');
 
 const RPC = "https://testnet.riselabs.xyz";
 const provider = new ethers.JsonRpcProvider(RPC);
@@ -10,28 +23,6 @@ const privateKeys = fs.readFileSync(path.join(__dirname, "privatekey.txt"), "utf
   .split("\n")
   .map(k => k.trim())
   .filter(k => k.length > 0);
-
-const WETH_ADDRESS = "0x4200000000000000000000000000000000000006";
-const USDT_ADDRESS = "0x40918Ba7f132E0aCba2CE4de4c4baF9BD2D7D849";
-const inari_ROUTER = "0x81edb206Fd1FB9dC517B61793AaA0325c8d11A23";
-const Withdraw_ROUTER = "0x832E537e88d0E8E5bb4eFb735F521A9A0E085e0A";
-
-const erc20_abi = [
-  "function approve(address spender, uint256 amount) external returns (bool)",
-  "function allowance(address owner, address spender) view returns (uint256)"
-];
-
-const inari_abi = [
-  "function supply(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external returns (bool)",
-  "function borrow(address asset, uint256 amount, uint256 interestRateMode, uint16 referralCode, address onBehalfOf) external returns (bool)",
-  "function repay(address asset, uint256 amount, uint256 interestRateMode, address onBehalfOf) external returns (bool)",
-  "function setUserUseReserveAsCollateral(address asset, bool useAsCollateral) external returns (bool)",
-  "function previewRedeem(uint256 shares) view returns (uint256 assets)",
-  "function balanceOf(address owner) view returns (uint256)",
-  "function withdraw(address asset, uint256 amount, address to) external returns (bool)"
-];
-
-const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 async function approve(wallet, fromTokenAddress, SPENDER, amountIn) {
   const token = new ethers.Contract(fromTokenAddress, erc20_abi, wallet);
@@ -161,26 +152,23 @@ async function withdrawETH(wallet, wdETHAmount) {
 
 async function main() {
   console.clear();
+  displayskw();
   
   for (const privateKey of privateKeys) {
     const wallet = new ethers.Wallet(privateKey, provider);
     console.log(chalk.cyan(`🔑 Wallet: ${wallet.address}\n`));
 
-    const supplyWETH = "0.0001";
-    const borrowUSDT = "0.001";
-    const wdETHAmount = "0.00001";
-
     await supply(wallet, supplyWETH);
-    await delay(5000);
+    await delay(3000);
 
     await borrow(wallet, borrowUSDT);
-    await delay(5000);
+    await delay(3000);
 
     await repay(wallet);
-    await delay(5000);
+    await delay(3000);
 
     await withdrawETH(wallet, wdETH);
-    await delay(5000);
+    await delay(3000);
   }
 }
 
